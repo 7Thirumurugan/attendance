bce759bb3d796b75e2dbb23d6b544e4372cdfd07
Thiru Murugan <thirumahi65@gmail.com>
2025-11-16 13:24:19 +0530

    Removed venv from tracking

--- 1_datasetCreation.py
+++ 1_datasetCreation.py
@@ -1,27 +1,99 @@
 
+# import imutils
+# import time
+# import cv2
+# import csv
+# import os
+
+
+# from app import db, Student, app
+
+
+# #cascade = 'haarcascade_frontalface_default.xml'
+
+# cascade = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+# detector = cv2.CascadeClassifier(cascade)
+
+# Name = str(input("Enter your Name : "))
+# Roll_Number = int(input("Enter your Roll_Number : "))
+# dataset = 'dataset'
+# sub_data = Name
+# path = os.path.join(dataset, sub_data)
+
+# # Insert into DB
+# with app.app_context():
+#     new_student = Student(name=Name, rollno=Roll_Number)
+#     db.session.add(new_student)
+#     db.session.commit()
+
+# if not os.path.isdir(path):
+#     os.mkdir(path)
+#     print(sub_data)
+
+# info = [str(Name), str(Roll_Number)]
+# with open('student.csv', 'a') as csvFile:
+#     write = csv.writer(csvFile)
+#     write.writerow(info)
+# csvFile.close()
+
+
+# print("Starting video stream...")
+# cam = cv2.VideoCapture(0)
+# time.sleep(8.0)
+# total = 0
+
+# while total < 25:
+#     print(total)
+#     _, frame = cam.read()
+#     img = imutils.resize(frame, width=400)
+#     rects = detector.detectMultiScale(
+#         cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), scaleFactor=1.1,
+#         minNeighbors=5, minSize=(30, 30))
+
+#     for (x, y, w, h) in rects:
+#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+#         p = os.path.sep.join([path, "{}.png".format(
+#             str(total).zfill(5))])
+#         cv2.imwrite(p, img)
+#         total += 1
+
+#     cv2.imshow("Frame", frame)
+#     key = cv2.waitKey(1) & 0xFF
+#     if key == ord("q"):
+#         break
+
+
+# cam.release()
+# cv2.destroyAllWindows()
+
+
+
+
+
 import imutils
 import time
 import cv2
 import csv
 import os
 
-
 from app import db, Student, app
 
-
-#cascade = 'haarcascade_frontalface_default.xml'
 cascade = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 detector = cv2.CascadeClassifier(cascade)
 
 Name = str(input("Enter your Name : "))
 Roll_Number = int(input("Enter your Roll_Number : "))
+
+# ADDED: Take phone number
+Phone_Number = str(input("Enter your Phone Number (+91XXXXXXXXXX) : "))
+
 dataset = 'dataset'
 sub_data = Name
 path = os.path.join(dataset, sub_data)
 
 # Insert into DB
 with app.app_context():
-    new_student = Student(name=Name, rollno=Roll_Number)
+    new_student = Student(name=Name, rollno=Roll_Number, phone=Phone_Number)  # ADDED phone
     db.session.add(new_student)
     db.session.commit()
 
@@ -29,12 +101,12 @@ if not os.path.isdir(path):
     os.mkdir(path)
     print(sub_data)
 
-info = [str(Name), str(Roll_Number)]
+# Save in CSV
+info = [str(Name), str(Roll_Number), str(Phone_Number)]   # ADDED phone
 with open('student.csv', 'a') as csvFile:
     write = csv.writer(csvFile)
     write.writerow(info)
 csvFile.close()
-
 
 print("Starting video stream...")
 cam = cv2.VideoCapture(0)
@@ -47,12 +119,12 @@ while total < 25:
     img = imutils.resize(frame, width=400)
     rects = detector.detectMultiScale(
         cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), scaleFactor=1.1,
-        minNeighbors=5, minSize=(30, 30))
+        minNeighbors=5, minSize=(30, 30)
+    )
 
     for (x, y, w, h) in rects:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        p = os.path.sep.join([path, "{}.png".format(
-            str(total).zfill(5))])
+        p = os.path.sep.join([path, "{}.png".format(str(total).zfill(5))])
         cv2.imwrite(p, img)
         total += 1
 
@@ -61,8 +133,5 @@ while total < 25:
     if key == ord("q"):
         break
 
-
 cam.release()
 cv2.destroyAllWindows()
-
-
